e1743bdd543b31138420211326d4803bc97a881c
Matthieu Mota <matthieumota@gmail.com>
2022-10-03 15:18:36 +0200

    Install Tailwind

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,7 +4,11 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 
 function App() {
-    return <h1>Laravel avec React</h1>;
+    return (
+        <div className="max-w-2xl mx-auto mt-8">
+            <h1 className="text-3xl text-center font-bold">Laravel avec React</h1>
+        </div>
+    );
 }
 
 createRoot(document.querySelector('#app')).render(<App />);
